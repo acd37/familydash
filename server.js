9eaa3f023038531bbc3187facdf19dbef23294a2
Alec Down <alecdown@gmail.com>
2019-10-22 16:33:52 -0500

    fixes __dirname typo

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ db.sequelize.sync({ alter: true }).then(() => {
   // server static assets if in production
   if (process.env.NODE_ENV === 'production') {
     // set static folder
-    app.use(express.static(path.join(dirname__, 'client/build')));
+    app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
