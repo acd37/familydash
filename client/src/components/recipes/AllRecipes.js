ee8321dd6e26f0fcbdc5c978846d4159929e83c4
Alec Down <alecdown@gmail.com>
2020-04-15 14:20:52 -0500

    visual updates on recipe cards

--- client/src/components/recipes/AllRecipes.js
+++ client/src/components/recipes/AllRecipes.js
@@ -3,43 +3,41 @@ import RecipeCard from './RecipeCard';
 import { connect } from 'react-redux';
 
 const styles = {
-  flexContainer: {
-    display: 'grid',
-    gridGap: 10,
-    justifyContent: 'flex-start',
-    gridTemplateColumns: 'repeat(auto-fit, minmax(300px, auto))'
-  }
+    flexContainer: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        flexWrap: 'wrap'
+    }
 };
 
 class AllRecipes extends Component {
-  render() {
-    let content;
+    render() {
+        let content;
 
-    if (this.props.recipes.length >= 1) {
-      content = this.props.recipes.map((recipe) => <RecipeCard key={recipe.id} recipe={recipe} />);
-    } else {
-      content = (
-        <div>
-          <p> Add some recipes to see them here.</p>
-        </div>
-      );
+        if (this.props.recipes.length >= 1) {
+            content = this.props.recipes.map(recipe => (
+                <RecipeCard key={recipe.id} recipe={recipe} />
+            ));
+        } else {
+            content = (
+                <div>
+                    <p> Add some recipes to see them here.</p>
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                <h1> All Recipes </h1>
+
+                <div style={styles.flexContainer}>{content}</div>
+            </div>
+        );
     }
-
-    return (
-      <div>
-        <h1> All Recipes </h1>
-
-        <div style={styles.flexContainer}>{content}</div>
-      </div>
-    );
-  }
 }
 
-const mapStateToProps = (state) => ({
-  recipes: state.recipes.recipes
+const mapStateToProps = state => ({
+    recipes: state.recipes.recipes
 });
 
-export default connect(
-  mapStateToProps,
-  {}
-)(AllRecipes);
+export default connect(mapStateToProps, {})(AllRecipes);
--- client/src/components/recipes/RecipeCard.js
+++ client/src/components/recipes/RecipeCard.js
@@ -1,11 +1,47 @@
 import React, { useEffect } from 'react';
-import Tile from '../common/Tile';
-import { Link } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { deleteRecipe, updateRecipe } from '../../actions/recipeActions';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: 300,
+        maxWidth: '90%',
+        marginRight: 30,
+        marginBottom: 30
+    },
+    media: {
+        height: 0,
+        paddingTop: '56.25%' // 16:9
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest
+        })
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)'
+    },
+    activeFavorite: {
+        color: 'red'
+    }
+}));
 
 const RecipeCard = props => {
+    const classes = useStyles();
     const dispatch = useDispatch();
+    const history = useHistory();
     const { recipe } = props;
 
     const handleDeleteRecipe = recipe => {
@@ -29,36 +65,30 @@ const RecipeCard = props => {
     };
 
     return (
-        <div>
-            <Tile>
-                <h3>
-                    <Link to={`/dashboard/recipes/recipe/${recipe.id}`}>{recipe.name}</Link>
-                </h3>
-
-                <p style={{ marginBottom: 0 }}>
-                    Calories: {recipe.calorieCount} | Time: {(recipe.cookTime / 60).toFixed(1)} hrs.
-                </p>
-                <div className='extra content' style={{ marginTop: 15 }}>
-                    {recipe.calendar ? (
-                        <button className='ui button' onClick={() => handleMealPlan(recipe)}>
-                            Remove from Meal Plan
-                        </button>
-                    ) : (
-                        <button className='ui button' onClick={() => handleMealPlan(recipe)}>
-                            Add to Meal Plan
-                        </button>
-                    )}
-
-                    <span
-                        className='right floated star'
-                        onClick={() => handleDeleteRecipe(recipe)}
-                        style={{ cursor: 'pointer' }}>
-                        <i className='trash icon'></i>
-                        Delete
-                    </span>
-                </div>
-            </Tile>
-        </div>
+        <Card className={classes.root}>
+            <CardHeader
+                style={{ cursor: 'pointer' }}
+                subheader={`Cook time: ${(recipe.cookTime / 60).toFixed(1)} hrs.`}
+                title={recipe.name}
+                onClick={() => history.push(`/dashboard/recipes/recipe/${recipe.id}`)}
+            />
+            <CardMedia
+                className={classes.media}
+                image='https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+                title='Paella dish'
+            />
+            <CardActions disableSpacing>
+                <IconButton aria-label='add to favorites'>
+                    <FavoriteIcon
+                        className={recipe.calendar && classes.activeFavorite}
+                        onClick={() => handleMealPlan(recipe)}
+                    />
+                </IconButton>
+                <IconButton aria-label='share'>
+                    <DeleteIcon onClick={() => handleDeleteRecipe(recipe)} />
+                </IconButton>
+            </CardActions>
+        </Card>
     );
 };
 
